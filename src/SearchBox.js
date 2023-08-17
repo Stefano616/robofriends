f0f4224dc85692df24b1e3b3fcd4f36d1c9da6cc
Stefano Rocchetti <stefanorocchetti4@gmail.com>
2023-08-18 00:26:40 +0200

    Add first react project

---
+++ src/SearchBox.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+const SearchBox = ({ searchChange }) => {
+    return (
+        <div className="pa2">
+            <input
+                className="pa3 ba b--red bg-lightest-yellow"
+                type="search"
+                placeholder='search robofriend'
+                onChange={searchChange}
+            />
+        </div>
+
+
+    );
+}
+
+export default SearchBox;
