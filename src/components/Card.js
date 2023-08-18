6bde2b212dc447d925749b72c990e7f47b830726
Stefano Rocchetti <stefanorocchetti4@gmail.com>
2023-08-18 15:16:35 +0200

    Changed cards dimensions

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const Card = ({ id, name, email }) => {
     return (
-        <div className="tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5 mw5">
             <img src={`https://robohash.org/new${id}?200x200`} alt="robot image" />
             <div>
                 <h2>{name}</h2>
